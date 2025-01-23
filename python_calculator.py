8f1bb9f0693ff6fd769a747a47065fd5fe8512ef
Godson Thomas <godsonthomas2006@gmail.com>
2025-01-24 00:09:01 +0530

    CRATED FUNCTION FOR THE OPERATORS

--- python_calculator.py
+++ python_calculator.py
@@ -1,36 +1,243 @@
-
+import csv
 import tkinter as tk
+
 
 def display_number(number):
     current_text = entry.get()
-    entry.insert(0, current_text + str(number))  # Append the number to the current text
+    entry.delete(0, tk.END)
+    entry.insert(0, str(current_text) + str(number))
+
+
+def addition():
+    global First_number
+    global operator
+    operator = "addition"
+    First_number = int(entry.get())
+    entry.delete(0, tk.END)
+
+
+def Substraction():
+    global First_number
+    global operator
+    operator = "Substraction"
+    First_number = int(entry.get())
+    entry.delete(0, tk.END)
+
+
+def Division():
+    global First_number
+    global operator
+    operator = "Division"
+    First_number = int(entry.get())
+    entry.delete(0, tk.END)
+
+
+def Multiplication():
+    global First_number
+    global operator
+    operator = "Multiplication"
+    First_number = int(entry.get())
+    entry.delete(0, tk.END)
+
+
+def Equal():
+    global First_number
+    global operator
+    Second_number = int(entry.get())
+    try:
+        if operator == "addition":
+            entry.delete(0, tk.END)
+            result = First_number + Second_number
+            entry.insert(0, result)
+
+        elif operator == "Substraction":
+            entry.delete(0, tk.END)
+            result = First_number - Second_number
+            entry.insert(0, result)
+        elif operator == "Division":
+            entry.delete(0, tk.END)
+            result = First_number / Second_number
+            entry.insert(0, result)
+        elif operator == "Multiplication":
+            entry.delete(0, tk.END)
+            result = First_number * Second_number
+            entry.insert(0, result)
+
+    except ValueError:
+        entry.insert(0, "Syntax Error")
+    except NameError:
+        entry.insert(0, "Math Error")
+    except ZeroDivisionError:
+        entry.insert(0, "Math Error")
+
 
 # Create the main window
 root = tk.Tk()
 root.title("Calculator")
 
-# Create an entry box to display the number
 entry = tk.Entry(root, width=40, borderwidth=10)
 entry.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
-# Create buttons for three different numbers
-button1 = tk.Button(root, text="1",width=7,height=3,borderwidth=5,pady=20, padx=20, command=lambda: display_number(1))
-button2 = tk.Button(root, text="2",width=7,height=3,borderwidth=5,pady=20, padx=20, command=lambda: display_number(2))
-button3 = tk.Button(root, text="3",width=7,height=3,borderwidth=5,pady=20, padx=20, command=lambda: display_number(3))
-button4 = tk.Button(root, text="4",width=7,height=3,borderwidth=5,pady=20, padx=20, command=lambda: display_number(4))
-button5 = tk.Button(root, text="5",width=7,height=3,borderwidth=5,pady=20, padx=20, command=lambda: display_number(5))
-button6 = tk.Button(root, text="6",width=7,height=3,borderwidth=5,pady=20, padx=20, command=lambda: display_number(6))
-button7 = tk.Button(root, text="7",width=7,height=3,borderwidth=5,pady=20, padx=20, command=lambda: display_number(7))
-button8 = tk.Button(root, text="8",width=7,height=3,borderwidth=5,pady=20, padx=20, command=lambda: display_number(8))
-button9 = tk.Button(root, text="9",width=7,height=3,borderwidth=5,pady=20, padx=20, command=lambda: display_number(9))
-button0 = tk.Button(root, text="0",width=7,height=3,borderwidth=5,pady=20, padx=20, command=lambda: display_number(0))
-button_clear = tk.Button(root, text="Clear",width=30,height=3,borderwidth=5,pady=20, padx=20,command=lambda:entry.delete(0,tk.END))
-button_add = tk.Button(root,text="+",width=7,height=3,borderwidth=5,pady=20, padx=20, command=lambda: display_number("+"))
-button_substraction = tk.Button(root,text="-",width=7,height=3,borderwidth=5,pady=20, padx=20, command=lambda: display_number("-"))
-button_division = tk.Button(root,text="/",width=7,height=3,borderwidth=5,pady=20, padx=20, command=lambda: display_number("/"))
-button_multiplication = tk.Button(root,text="x",width=7,height=3,borderwidth=5,pady=20, padx=20, command=lambda: display_number("x"))
-button_equal = tk.Button(root, text="=",width=7,height=3,borderwidth=5,pady=20, padx=20, command=lambda: display_number("="))
-                                         
+button1 = tk.Button(
+    root,
+    text="1",
+    width=7,
+    height=3,
+    borderwidth=5,
+    pady=20,
+    padx=20,
+    command=lambda: display_number(1),
+)
+button2 = tk.Button(
+    root,
+    text="2",
+    width=7,
+    height=3,
+    borderwidth=5,
+    pady=20,
+    padx=20,
+    command=lambda: display_number(2),
+)
+button3 = tk.Button(
+    root,
+    text="3",
+    width=7,
+    height=3,
+    borderwidth=5,
+    pady=20,
+    padx=20,
+    command=lambda: display_number(3),
+)
+button4 = tk.Button(
+    root,
+    text="4",
+    width=7,
+    height=3,
+    borderwidth=5,
+    pady=20,
+    padx=20,
+    command=lambda: display_number(4),
+)
+button5 = tk.Button(
+    root,
+    text="5",
+    width=7,
+    height=3,
+    borderwidth=5,
+    pady=20,
+    padx=20,
+    command=lambda: display_number(5),
+)
+button6 = tk.Button(
+    root,
+    text="6",
+    width=7,
+    height=3,
+    borderwidth=5,
+    pady=20,
+    padx=20,
+    command=lambda: display_number(6),
+)
+button7 = tk.Button(
+    root,
+    text="7",
+    width=7,
+    height=3,
+    borderwidth=5,
+    pady=20,
+    padx=20,
+    command=lambda: display_number(7),
+)
+button8 = tk.Button(
+    root,
+    text="8",
+    width=7,
+    height=3,
+    borderwidth=5,
+    pady=20,
+    padx=20,
+    command=lambda: display_number(8),
+)
+button9 = tk.Button(
+    root,
+    text="9",
+    width=7,
+    height=3,
+    borderwidth=5,
+    pady=20,
+    padx=20,
+    command=lambda: display_number(9),
+)
+button0 = tk.Button(
+    root,
+    text="0",
+    width=7,
+    height=3,
+    borderwidth=5,
+    pady=20,
+    padx=20,
+    command=lambda: display_number(0),
+)
+button_clear = tk.Button(
+    root,
+    text="Clear",
+    width=30,
+    height=3,
+    borderwidth=5,
+    pady=20,
+    padx=20,
+    command=lambda: entry.delete(0, tk.END),
+)
+button_add = tk.Button(
+    root,
+    text="+",
+    width=7,
+    height=3,
+    borderwidth=5,
+    pady=20,
+    padx=20,
+    command=lambda: addition(),
+)
+button_substraction = tk.Button(
+    root,
+    text="-",
+    width=7,
+    height=3,
+    borderwidth=5,
+    pady=20,
+    padx=20,
+    command=lambda: Substraction(),
+)
+button_division = tk.Button(
+    root,
+    text="/",
+    width=7,
+    height=3,
+    borderwidth=5,
+    pady=20,
+    padx=20,
+    command=lambda: Division(),
+)
+button_multiplication = tk.Button(
+    root,
+    text="x",
+    width=7,
+    height=3,
+    borderwidth=5,
+    pady=20,
+    padx=20,
+    command=lambda: Multiplication(),
+)
+button_equal = tk.Button(
+    root,
+    text="=",
+    width=7,
+    height=3,
+    borderwidth=5,
+    pady=20,
+    padx=20,
+    command=lambda: Equal(),
+)
 
 
 # Put the buttons on the screen
@@ -46,11 +253,10 @@ button9.grid(row=3, column=2)
 button0.grid(row=4, column=0)
 button_add.grid(row=4, column=1)
 button_substraction.grid(row=4, column=2)
-button_division .grid(row=5, column=0)
+button_division.grid(row=5, column=0)
 button_multiplication.grid(row=5, column=1)
 button_equal.grid(row=5, column=2)
-button_clear.grid(row=6, column=0,columnspan=4)
-
+button_clear.grid(row=6, column=0, columnspan=4)
 
 
 # Start the main event loop
